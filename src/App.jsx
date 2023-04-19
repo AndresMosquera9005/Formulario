b0660c93789b7dd97dcc06d77863f465276c56e6
javier mosquera <andrubjj@gmail.com>
2023-04-19 10:33:41 -0500

    5 actualizaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
 import { useState } from "react"
+import "../src/App.scss"
 
-const App =() => {
+const App = () => {
 
-  const [num, setNum] = useState(0)
-  const [mail, setMail] =useState("")
+  const [data, setData] = useState({ email: "", password: ""})
+
+  const changeData = e => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
   
-  const calculateLong = e => {
-    setNum(e.target.value.length)
+  const submit = e =>{
+    e.preventDefault()
+    console.log(data);
+    alert("Bienvenido Luis Felipiño al formulario en React ")
   }
 
-  const changeEmail = e => setMail(e.target.Value)
-
-
-  return (
-    <div>
-      <form>        
-        <input 
-          type="email" 
-          Value={mail}
-          onChange={changeEmail}
-        />
-        <input 
-          type="password"
-          placeholder="Contraseña" 
-          required 
-          onChange={calculateLong}
-        />
-        <p>Usted ingresò {num} caracteres</p>
-
-        <button type="submit">Registrarse</button>
+  return(
+    <div className="ed-grid" >
+      <form className="ed-container l-30" onSubmit={submit}>
+        <div className="ed-item form__item">
+          <label htmlFor="email">
+            Correo electronico
+            <input
+              type="email" 
+              name="email" 
+              id="email" 
+              value={data.email} 
+              onChange={changeData}
+            />
+          </label>
+        </div>
+        <div className="ed-item form__item">
+          <label htmlFor="password">
+            Contraseña
+            <input 
+              type="password" 
+              name="password" 
+              id="password" 
+              value={data.password} 
+              onChange={changeData}
+            />
+          </label>
+        </div>
+        <div className="ed-item form__item">
+          <input type="submit" value="Ingresar" className="button full" />
+        </div>
       </form>
     </div>
   )
